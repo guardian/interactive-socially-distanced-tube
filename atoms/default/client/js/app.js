f72b159a4dcf4bda2caa2a7c45d620cf5239cb41
Anna Leach <anna.leach@guardian.co.uk>
2020-05-06 21:28:11 +0100

    basics working

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -3,18 +3,27 @@ import gsap from "gsap";
 // Canvas consts
 const canvas = document.querySelector("#normal");
 const ctx = canvas.getContext("2d");
-const w = 400;
+const clock = document.querySelector("#clock");
+const w = 700;
 const h = 300;
-const stationW = 300;
+const stationW = 600;
 const trainW = w - stationW;
-const yellowLineW = 15;
-const trainX = w - ((trainW - yellowLineW) / 2); // x position of the middle of the "train";
+const yellowLineOffset = 15;
+const yellowLineWidth = 10;
+const trainOffset = 20;
+const trainEdge = w - (trainW - yellowLineOffset) + trainOffset; // x position of the edge of train plus 10 padding;
+const spaceInTrain = trainW - (trainOffset * 2) - yellowLineOffset - yellowLineWidth;
+
+// CHANGE THESE VARIABLES //
+const numParticles = 1005;
+const dotsPerTrain = 7;
+const dotsPerTrainNormal = 42;
 
 // Particle consts
-const numParticles = 10;
-const particleRadius = 7;
+const particleRadius = 5;
 const particleOffset = 10; //offset from top;
 const particleColor = "navy";
+const angryParticleColor = "red";
 
 const particleSpacing = particleRadius * 2 + 0.5 * particleRadius;
 const numParticlesInCol = Math.floor((h - particleOffset * 2) / particleSpacing);
@@ -22,30 +31,50 @@ const numColsInStation = Math.floor((stationW - particleOffset * 2) / particleSp
 
 // Animation consts 
 const timeToStation = 5;
+const timeToStationMs = timeToStation * 1000
 const waitForTrain = 2;
-const trainsArriving = 6;  // 6 in a 15 min period
-const dotsPerTrain = 3;
+const trainInterval = 2; 
+const numberOfTrains = 24;
+let currentTrain = 1;  //modified below
 
-//assign each dot a train number depending on number of trains arriving and dots per train
-// for each dot - index 1 goes to train 1 unless over dotsperttrain, what bracket does index fall into.
+// Timer consts 
+const totalTime = numberOfTrains * trainInterval; // NB not including time to station
+const timePerMin = (totalTime / 60) * 1000; // time in milliseconds for each tick of the clock 
+let clockIntrvl;
+let clockDelay;
+let clockHour = 8;
+let clockMin = 0;
 
 // set up canvas
 canvas.width = w;
 canvas.height = h;
 
-// set up train area
 const setUpStation = (ctx) => {
+    //draw train area
     ctx.fillStyle = "#FF7F7F";
-    ctx.fillRect((stationW + 15), 0, (trainW - 15), h);
+    ctx.fillRect((stationW + yellowLineOffset), 0, (trainW - 15), h);
     
+    // draw yellow line
     ctx.fillStyle = "#e5e500";
-    ctx.fillRect(stationW, 0, 10, h);
+    ctx.fillRect(stationW, 0, yellowLineWidth, h);
 }
 
+const tickClock = () => {
+    clock.textContent = `${clockHour}:${clockMin < 10 ? '0' + clockMin : clockMin}`;
+    if(clockMin === 59) {
+        clockMin = 0; 
+        clockHour = 9;
+    }
+    if(clockHour === 9){
+       tickClock();
+       clearInterval(clockIntrvl);
+       clearTimeout(clockDelay);
+    }
+    clockMin++;
+}
 
 const calcDotStationPos = (i) => {
-    // y needs to go from the start of the column 
-    // we want it reversed so dots build up from the bottom 
+    // y needs to go from the start of the column, we want it reversed so dots build up from the bottom 
     let indexInCol = numParticlesInCol - (i % numParticlesInCol);
     // start at the side closest to the train
     let colIndex = Math.floor(i / numParticlesInCol)
@@ -56,32 +85,42 @@ const calcDotStationPos = (i) => {
     return {x, y}; 
 }
 
-const calcDotTrain = (indx) => {
-    let selectedTrain = indx < dotsPerTrain ? 1 : Math.round(indx / dotsPerTrain);
-    return selectedTrain;
+const calcDotTrain = (indx) => Math.ceil((indx + 1) / dotsPerTrain); // add one to avoid zero-index
+
+const calcBoardTrainDelay = (train, startDelay, travelTime) => {
+    const baseDelay = timeToStation - startDelay - travelTime;
+    return baseDelay + (train * trainInterval); 
 }
 
+const calcTrainPos = trainEdge + Math.random() * spaceInTrain;
+
+
 class Particle {
-    constructor(startX, startY, midX, midY, startDelay, train) {
+    constructor(startX, startY, midX, midY, startDelay, train, index) {
         this.x = startX;
         this.y = startY;
         this.midX = midX;
         this.midY = midY;
         this.train = train;
-        this.travelTime = Math.random(timeToStation - waitForTrain); // should be 3
+        this.index = index;
+        this.gettingOn = train <= numberOfTrains;
+        this.travelTime = Math.random() * (timeToStation - waitForTrain);
         this.animation = gsap.timeline()
           .to(this, this.travelTime, {
             x: this.midX,
             y: this.midY,
-            delay: startDelay
-        })
+            delay: startDelay,
+            ease: "rough"
+          })
           .to(this, 0.5, {
-            x: trainX,
-            delay: timeToStation - startDelay - this.travelTime
+            x: calcTrainPos(this.midX),
+            delay: calcBoardTrainDelay(this.train, startDelay, this.travelTime), //max out at total time.
+            ease: "power1.inOut"
           })
           .to(this, 1, {
-            y: (h - this.midY) * -1,
-            delay: 0.5
+            y: this.gettingOn ? (h - this.midY) * -1 : this.midY + 3, // move them out at a steady pace ,each covers the same distance
+            delay: 0.5,
+            ease: this.gettingOn ? "power2.in" : "steps",
           })
     }
     draw (ctx){
@@ -103,18 +142,22 @@ let startDelayInterval = (timeToStation - waitForTrain) / numParticles;
 for (let i = 0; i < numParticles; i++) {
     let {x, y} = calcDotStationPos(i);
     let train = calcDotTrain(i);
-    arrParticles.push(new Particle(particleOffset, particleOffset, x, y, startDelay, train));
+    arrParticles.push(new Particle(particleOffset, particleOffset, x, y, startDelay, train, i));
     startDelay += startDelayInterval;
+    train > currentTrain && currentTrain++; // bump currentTrain if previous ran out of space
 }
-
-console.log(arrParticles);
 
 const render = () => {
     ctx.clearRect(0, 0, w, h);
     setUpStation(ctx);
+
     arrParticles.forEach((particle) => {
         particle.draw(ctx);
     })
+    
 }
+//wait till everyone is at the station, then start ticker;
+const startClock = () => clockIntrvl = setInterval(tickClock, timePerMin);
+clockDelay = window.setTimeout(startClock, timeToStationMs);
 
 gsap.ticker.add(render);
